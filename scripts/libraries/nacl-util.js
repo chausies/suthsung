3820314da463eed2f9801277eae63fa8f9ccc057
chausies <chausies@berkeley.edu>
2020-06-30 03:14:46 -0700

    Stripped down nacl-util

---
+++ scripts/libraries/nacl-util.js
@@ -0,0 +1,25 @@
+// Heavily stripped down from original
+(function() {
+  var util = {};
+
+  function validateBase64(s) {
+    if (!(/^(?:[A-Za-z0-9+\/]{2}[A-Za-z0-9+\/]{2})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/.test(s))) {
+      throw new TypeError('invalid encoding');
+    }
+  }
+
+  util.encodeBase64 = function(arr) {
+    var i, s = [], len = arr.length;
+    for (i = 0; i < len; i++) s.push(String.fromCharCode(arr[i]));
+    return btoa(s.join(''));
+  };
+
+  util.decodeBase64 = function(s) {
+    validateBase64(s);
+    var i, d = atob(s), b = new Uint8Array(d.length);
+    for (i = 0; i < d.length; i++) b[i] = d.charCodeAt(i);
+    return b;
+  };
+
+  nacl.util = util;
+})();
