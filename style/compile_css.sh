8ffab15f80a5a60977547c8ef118eb0e17db1879
chausies <chausies@berkeley.edu>
2020-07-07 02:16:13 -0700

    Updated css compilation to use index.html

--- style/compile_css.sh
+++ style/compile_css.sh
@@ -1,5 +1,5 @@
 # rm main.min.css
 # rm temp.css
 lessc main.less temp.css
-purifycss temp.css ../suthsung.html ../html/*.html ../scripts/suthsung.min.js --min --out main.min.css
+purifycss temp.css ../index.html ../html/*.html ../scripts/suthsung.min.js --min --out main.min.css
 rm temp.css
